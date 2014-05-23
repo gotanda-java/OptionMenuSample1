8926ac27b615468ec3aa5e8f1b71cd7a196ae54a
7799 <hayashi7799@gmail.com>
2014-05-23 13:24:23 +0900

    todo: check apps  launcher base

--- src/example/android/optionmenusample/MainActivity.java
+++ src/example/android/optionmenusample/MainActivity.java
@@ -1,11 +1,22 @@
 package example.android.optionmenusample;
 
+import java.io.File;
+import java.util.List;
+import java.util.Stack;
+
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.ResolveInfo;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
@@ -17,8 +28,22 @@ public class MainActivity extends Activity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
+//		 Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
+
+
+
+
+//		super.onCreateOptionsMenu(menu);
+//		MenuItem item1 = menu.add(0,0,0, "item1");
+//		item1.setIcon(android.R.drawable.ic_menu_compass);
+//		MenuItem item2 = menu.add(0,1,0, "item2");
+//		item2.setIcon(android.R.drawable.ic_menu_compass);
+//		MenuItem item3 = menu.add(0,2,0, "item3");
+//		item3.setIcon(android.R.drawable.ic_menu_compass);
+//		MenuItem item4 = menu.add(0,3,0, "item4");
+//		item4.setIcon(android.R.drawable.ic_menu_compass);
+
 		return true;
 	}
 
@@ -26,14 +51,38 @@ public class MainActivity extends Activity {
 	public boolean onOptionsItemSelected(MenuItem item) {
 
 		switch (item.getItemId()) {
+//		case 0:
+////			showDialog("メニューアイテム１を選択しました。");
+//			return true;
+//		case 1:
+//			tel();
+////			showDialog("メニューアイテム電話を選択しました。");
+//			return true;
+//		case 2:
+//			email();
+////			showDialog("メニューアイテムEmailを選択しました。");
+//			return true;
+//		case 3:
+//			map();
+////			showDialog("メニューアイテムマップを選択しました。");
+//			return true;
+//			default:
+//				return super.onOptionsItemSelected(item);
+
 		case R.id.item1:
-			showDialog("メニューアイテム１を選択しました。");
+//			showDialog("メニューアイテム１を選択しました。");
 			return true;
 		case R.id.item2:
-			showDialog("メニューアイテム２を選択しました。");
+			tel();
+//			showDialog("メニューアイテム電話を選択しました。");
 			return true;
 		case R.id.item3:
-			showDialog("メニューアイテム３を選択しました。");
+			email();
+//			showDialog("メニューアイテムEmailを選択しました。");
+			return true;
+		case R.id.item4:
+			map();
+//			showDialog("メニューアイテムマップを選択しました。");
 			return true;
 			default:
 				return super.onOptionsItemSelected(item);
@@ -41,6 +90,39 @@ public class MainActivity extends Activity {
 		}
 
 //		return super.onOptionsItemSelected(item);
+	}
+
+
+
+	private void tel() {
+		// TODO 自動生成されたメソッド・スタブ
+//		Intent telIntent = new Intent(android.content.Intent.ACTION_DIAL);
+//		startActivity(Intent.createChooser(telIntent, null));
+
+		String action = "android.intent.action.DIAL";
+		String contentStr = null;
+		Intent intent = new Intent(action, Uri.parse(contentStr));
+		startActivity(intent);
+	}
+
+	private void email() {
+
+		Intent emailIntent = new Intent(android.content.Intent.ACTION_SENDTO);
+		emailIntent.setType("plain/text");
+//		startActivity(emailIntent);
+//		startActivity(Intent.createChooser(emailIntent, "Send your email in:"));
+		startActivity(Intent.createChooser(emailIntent, "Send your email in:"));
+		    }
+
+	private void map() {
+		// TODO 自動生成されたメソッド・スタブ
+//		Intent mapIntent = new Intent(android.content.Intent.ACTION_VIEW);
+//		startActivity(Intent.createChooser(mapIntent, null));
+
+		String action = "android.intent.action.VIEW";
+		String contentStr = null;
+		Intent intent = new Intent(action, Uri.parse(contentStr));
+		startActivity(intent);
 	}
 
 	private void showDialog(String text) {
@@ -55,7 +137,6 @@ public class MainActivity extends Activity {
 			public void onClick(DialogInterface dialog, int whichButton) {
 				// TODO 自動生成されたメソッド・スタブ
 				MainActivity.this.setResult(Activity.RESULT_OK);
-
 			}
 		});
 		dialog.create();
